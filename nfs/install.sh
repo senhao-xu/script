51ac1ca1cd84bcdf796038cb355f0430cb1261ed
senhao-xu <xusenhao1123@163.com>
2022-05-23 10:34:18 +0800

    update

--- nfs/install.sh
+++ nfs/install.sh
@@ -5,8 +5,11 @@ out(){
  echo -e "\033[42m$1\033[0m"
 }
 
-read -p "$(echo -e "ip:")" ip
-read -p "$(echo -e "path:")" path
+read -r -p '请输入ip(默认:'`ifconfig  eth0 | head -n2 | grep inet | awk '{print$2}'`'):' ip
+[ -z "${ip}" ] && ip="`ifconfig  eth0 | head -n2 | grep inet | awk '{print$2}'`"
+read -r -p '请输入挂载路径(默认:/data/nfs): ' path
+[ -z "${path}" ] && path="/data/nfs"
+
 
 mkdir -p $path
 
