9e761cda445d3a99aa096e0d4030b4fb963d5315
senhao <47684791+senhao-xu@users.noreply.github.com>
2024-07-04 19:51:02 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 #https://mirrors.cloud.tencent.com/
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos7_base.repo
+yum clean all
+yum makecache
+
 
 yum update -y 
 yum install -y vim wget net-tools.x86_64 bind-utils
