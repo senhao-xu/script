7cde7eb0767934987a83005d2edeca94160cb593
senhao <47684791+senhao-xu@users.noreply.github.com>
2024-07-04 14:01:05 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+sudo yum-config-manager --add-repo http://mirrors.tencentyun.com/repo/centos7_base.repo
+sudo yum-config-manager --add-repo http://mirrors.tencentyun.com/repo/centos7_epel.repo
+
 yum update -y 
 yum install -y vim wget net-tools.x86_64 bind-utils
 
