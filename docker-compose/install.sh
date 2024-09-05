16f35e3245306d1d95a2b6dc51adac3b087e536c
senhao-xu <xusenhao1123@163.com>
2024-09-05 21:35:48 +0800

    update

--- docker-compose/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CRTDIR=$(pwd)
-mv $CRTDIR/docker-compose /usr/local/bin
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
--- harbor/on-line/harbor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tar -zxvf harbor-online-installer-v2.4.1.tgz -C /usr/local/
-cp harbor.yml /usr/local/harbor
-sh /usr/local/harbor/install.sh
